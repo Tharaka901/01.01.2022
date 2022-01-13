7a87ed29af487d96ddc0c5c7bd0218b0f217dcce
Tharaka901 <tharakamihiri@gmail.com>
2022-01-13 13:03:16 +0530

    heroku

--- index.php
+++ index.php
@@ -16,6 +16,9 @@
         echo $name;
         echo "<h1>My Name is : $name <br/> My age: $age</h1>"
     ?>
+    <button type="button" class="btn btn-dark">CLICK ME!</Button>
     <button type="button" class="btn btn-primary">CLICK ME!</Button>
+    <button type="button" class="btn btn-success">CLICK ME!</Button>
+    <a href ="https://www.heroku.com">Heroku.com</a>
      
     <?php require "includes/footer.php" ?> 
